2a9fb90eb80cfe1cb58ec5dda75619783d775279
Huseyinenesyilmaz07 <bubenimgizlimailim@gmail.com>
2026-08-04 22:11:08 +0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     },
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss() as any],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
